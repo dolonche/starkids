8eee33d094312986c27d1096d2887339a4a1cd67
Иван Питенин <ivan.pitenin@gmail.com>
2020-02-16 16:29:20 +0300

    start + complete 2 screens

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'slick-carousel';
+import './pisos.js'
+for (let i = 0; i < 20; i++) {
+	console.log(i);
+};
---
+++ src/js/pisos.js
@@ -0,0 +1,8 @@
+// document.addEventListener('click', function() {
+// 	if (document.querySelector('header').style.backgroundColor === 'rgb(64, 224, 208)') {
+// 		document.querySelector('header').style.backgroundColor = '#ffc0cb';
+// 	} else {
+// 		document.querySelector('header').style.backgroundColor = 'rgb(64, 224, 208)';
+// 	}
+// 	console.log('pisos3309809');
+// })
