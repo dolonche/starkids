09609c48a39bf338d6dd071950a96151288a86f1
Иван Питенин <ivan.pitenin@gmail.com>
2020-02-23 17:20:35 +0300

    add popups and animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,16 @@
-import $ from 'jquery';
-import 'slick-carousel';
-import './pisos.js'
-for (let i = 0; i < 20; i++) {
-	console.log(i);
-};
+import './animatescroll';
+$(document).ready(() => {
+	$(document).click((e) => {
+		console.log(e.target);
+		if (e.target.dataset.popup === "form") {
+			$('.overlay')[0].classList.toggle('overlay--open');
+			$('.popup-form')[0].classList.toggle('popup-form--open');
+			$('body')[0].classList.toggle('page-body--overlay');
+		}
+		if (e.target.classList.contains('popup-form__close') || e.target.classList.contains('overlay--open')) {
+			$('.overlay--open')[0].classList.toggle('overlay--open');
+			$('.popup-form--open')[0].classList.toggle('popup-form--open');
+			$('body')[0].classList.toggle('page-body--overlay');
+		}
+	});
+});
